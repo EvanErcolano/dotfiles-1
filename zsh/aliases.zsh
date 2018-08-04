4038fa27d560e8746a44e00f90d1bd0972bab345
Evan Ercolano <evanercolano@gmail.com>
2018-08-04 00:44:51 -0400

    Remoe uneeded bits

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,27 @@
+# Reload zsh
 alias reload!='. ~/.zshrc'
 
-alias cls='clear' # Good 'ol Clear Screen command
+# Allow aliases to be used with sudo
+alias sudo="sudo "
 
-alias history='history 1'
+# Navigation
+alias ..="cd .."
+
+# Enable colored grep output
+alias grep='grep --color=auto'
+
+# list files setings
+alias ls='ls -G'
+alias ll='ls -AlhFG'
+
+# Access hosts file
+alias hosts='sudo vim /etc/hosts'
+
+# Clear screen
+alias c='clear'
+
+# Open google chrome with a particular url
+alias chrome="open -a 'Google Chrome'"
+
+# Command Frequency
+alias cmdfreq='cat ~/.bash_history | cut -d " " -f1 | sort | uniq -c | sort -g | sort -rn | head'
